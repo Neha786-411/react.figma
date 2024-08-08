3b3ff8365db2533657c7a45d6af2c6d1398fadfb
Neha786-411 <NN5902722@GMAIL.COM>
2024-08-08 16:33:30 +0530

    complete project

---
+++ hello.js
@@ -0,0 +1,296 @@
+import React from "react";
+import "./sss.css"
+
+const MainPage = () => {
+  return (
+    <div className="al">
+      <div classNameName="main">
+        <div className="main1">
+          <div className="ch" >
+            {/* <img
+              src="./images/Group_1.svg"
+              alt="icon"
+            /> */}
+
+            <span className="border">
+              {" "}
+              <b> Chatterly</b>
+            </span>
+          </div>
+          <div className="sahil">
+            <div className="sahil-img border"></div>
+            <div className="sahil-sharma border">
+              <select>
+                <option>Sahil sharma</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <hr className="border1"></hr>
+        <div className="main2">
+          <div className="main3">
+            <div className="filter">Filter</div>
+
+            <div className="created">
+              <p>Created by</p>
+              <select className="border">
+                <option>All</option>
+              </select>
+            </div>
+            <div className="published">
+              <p>Published By</p>
+              <select className="border">
+                <option>Select Date</option>
+              </select>
+            </div>
+          </div>
+          <div className="search">
+            <p>
+              <u className="under">Search</u>
+            </p>
+            <input className="border" placeholder="Type to search" />
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+      </div>
+
+      <div className="main4">
+        <div className="blog">Blog Post</div>
+        <div className="div1">
+          <div className="container">
+            <div className="img-box border">
+              <div className="img1 dd border"></div>
+              <p className="para">
+                How to make GUI in Java with <br /> example example
+              </p>
+              <hr className="border1" />
+              <div className="circle-main">
+                <div className="cir-img border">
+                  <div className="dasteen">DASTEEN </div>
+                </div>
+                <div className="date">
+                  <p>2 july 2022</p>
+                  <i className="fa-regular fa-heart "></i>
+                  <p>03</p>
+                  <i className="fa-regular fa-comment"></i>
+                  <p>04</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="img-box border">
+              <div className="img2 dd border"></div>
+              <p className="para">
+                How to make GUI in Java with <br /> example example
+              </p>
+              <hr className="border1" />
+              <div className="circle-main">
+                <div className="cir-img border">
+                  <div className="dasteen">DASTEEN </div>
+                </div>
+                <div className="date">
+                  <p>2 july 2022</p>
+                  <i className="fa-regular fa-heart "></i>
+                  <p>03</p>
+                  <i className="fa-regular fa-comment"></i>
+                  <p>04</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="img-box border">
+              <div className="img3 dd border"></div>
+              <p className="para">
+                How to make GUI in Java with <br /> example example
+              </p>
+              <hr className="border1" />
+              <div className="circle-main">
+                <div className="cir-img border">
+                  <div className="dasteen">DASTEEN </div>
+                </div>
+                <div className="date">
+                  <p>2 july 2022</p>
+                  <i className="fa-regular fa-heart "></i>
+                  <p>03</p>
+                  <i className="fa-regular fa-comment"></i>
+                  <p>04</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="div1">
+            <div className="container">
+              <div className="img-box border">
+                <div className="img1 dd border"></div>
+                <p className="para">
+                  How to make GUI in Java with <br /> example example
+                </p>
+                <hr className="border1" />
+                <div className="circle-main">
+                  <div className="cir-img border">
+                    <div className="dasteen">DASTEEN </div>
+                  </div>
+                  <div className="date">
+                    <p>2 july 2022</p>
+                    <i className="fa-regular fa-heart "></i>
+                    <p>03</p>
+                    <i className="fa-regular fa-comment"></i>
+                    <p>04</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="img-box border">
+                <div className="img2 dd border"></div>
+                <p className="para">
+                  How to make GUI in Java with <br /> example example
+                </p>
+                <hr className="border1" />
+                <div className="circle-main">
+                  <div className="cir-img border">
+                    <div className="dasteen">DASTEEN </div>
+                  </div>
+                  <div className="date">
+                    <p>2 july 2022</p>
+                    <i className="fa-regular fa-heart "></i>
+                    <p>03</p>
+                    <i className="fa-regular fa-comment"></i>
+                    <p>04</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="img-box border">
+                <div className="img3 dd border"></div>
+                <p className="para">
+                  How to make GUI in Java with <br /> example example
+                </p>
+                <hr className="border1" />
+                <div className="circle-main">
+                  <div className="cir-img border">
+                    <div className="dasteen">DASTEEN </div>
+                  </div>
+                  <div className="date">
+                    <p>2 july 2022</p>
+                    <i className="fa-regular fa-heart "></i>
+                    <p>03</p>
+                    <i className="fa-regular fa-comment"></i>
+                    <p>04</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="div1">
+              <div className="container">
+                <div className="img-box border">
+                  <div className="img1 dd border"></div>
+                  <p className="para">
+                    How to make GUI in Java with <br /> example example
+                  </p>
+                  <hr className="border1" />
+                  <div className="circle-main">
+                    <div className="cir-img border">
+                      <div className="dasteen">DASTEEN </div>
+                    </div>
+                    <div className="date">
+                      <p>2 july 2022</p>
+                      <i className="fa-regular fa-heart "></i>
+                      <p>03</p>
+                      <i className="fa-regular fa-comment"></i>
+                      <p>04</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="img-box border">
+                  <div className="img2 dd border"></div>
+                  <p className="para">
+                    How to make GUI in Java with <br /> example example
+                  </p>
+                  <hr className="border1" />
+                  <div className="circle-main">
+                    <div className="cir-img border">
+                      <div className="dasteen">DASTEEN </div>
+                    </div>
+                    <div className="date">
+                      <p>2 july 2022</p>
+                      <i className="fa-regular fa-heart "></i>
+                      <p>03</p>
+                      <i className="fa-regular fa-comment"></i>
+                      <p>04</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="img-box border">
+                  <div className="img3 dd border"></div>
+                  <p className="para">
+                    How to make GUI in Java with <br /> example example
+                  </p>
+                  <hr className="border1" />
+                  <div className="circle-main">
+                    <div className="cir-img border">
+                      <div className="dasteen">DASTEEN </div>
+                    </div>
+                    <div className="date">
+                      <p>2 july 2022</p>
+                      <i className="fa-regular fa-heart "></i>
+                      <p>03</p>
+                      <i className="fa-regular fa-comment"></i>
+                      <p>04</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p className="kl"></p>
+        </div>
+      </div>
+
+      <footer>
+        <div className="foot">
+          <div className="foot1">
+            <div className="foot2">
+              <span className="train">
+                Training <a>Blog</a>
+              </span>
+              <span className="html1">
+                Welcome to our technical blog, where we delve into the <br />{" "}
+                world of cutting-edge technologies and explore their <br />{" "}
+                practical applications.
+              </span>
+            </div>
+            <div className="foot2">
+              <span className="cate">Category</span>
+              <span className="html">HTML</span>
+              <span className="html">CSS</span>
+              <span className="html">JAVASCRIPT</span>
+              <span className="html">VSCODE</span>
+            </div>
+            <div className="foot2">
+              <p className="cate">Get in touch</p>
+              <p className="html">+91-8XXX-XXX-XX</p>
+              <p className="html">Demo@gMAIL.COM</p>
+            </div>
+            <div className="foot2">
+              <span className="cate">FOLLOW US ON</span>
+              <span>
+                <i className="fa-brands fa-twitter html "></i>
+                <i className="fa-brands fa-instagram html"></i>
+                <i className="fa-brands fa-linkedin-in html"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <hr className="fo-hr border1" />
+        <div className="last">
+          <p className="lastp">© 2023 tRAININGbLOG</p>
+          <p className="last2">Made With ❤️ Mohali, india</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default MainPage;
